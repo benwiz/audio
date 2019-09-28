57267d93b3b58ff84997cdc8e4ec3e9c4a9297d3
Ben Wiz <bwisialowski@gmail.com>
2019-09-27 20:54:30 -0500

    generate threads with data

--- looper.rb
+++ looper.rb
@@ -7,7 +7,9 @@
 # TODO: Rather than storing just the buffer_id, store a
 # map with a `play?` boolean.
 # TODO: play_audio should honor the play boolean
-# TODO: OSC command to set the play boolean (or toggle if no args)
+# TODO: OSC command to set the play boolean (or toggleif no args)
+# TODO: Add metronome and related osc commands
+# TODO: Output information about the buffers via osc_send
 
 require 'securerandom'
 
@@ -42,8 +44,9 @@ set :buffer_ids, []
 
 # Generate a new id and add it to the
 # global list of buffer ids. Return
-# new id.
-define :new_buffer_id do
+# new id. By using UUID there is no risk
+# of using an old cached buffer.
+def new_buffer_id()
   id = SecureRandom.uuid.to_sym
   buffer_ids = get(:buffer_ids)
   buffer_ids += [id]
@@ -52,12 +55,12 @@ define :new_buffer_id do
 end
 
 
-define :kick do
+def kick()
   sample :bd_haus, rate: 1
 end
 
 
-define :start_record_audio do
+def start_record_audio()
   # Create a new buffer
   id = new_buffer_id
   buf = buffer(id, buffer_size)
@@ -68,25 +71,18 @@ define :start_record_audio do
 end
 
 
-define :stop_record_audio do
+def stop_record_audio()
   live_audio :live_audio_synth, :stop
 end
 
 
-define :play_audio do
+def play_audio()
   buffer_ids = get(:buffer_ids)
   buffer_ids.each do |id|
     sample buffer(id, buffer_size)
   end
 end
 
-
-# FIXME
-define :logbuffers do
-  buffer_ids = get(:buffer_ids)
-  puts "buffer_ids::"
-  puts buffer_ids
-end
 
 
 #####################
@@ -95,48 +91,64 @@ end
 ##|               |##
 #####################
 
-in_thread do
-  live_loop :kick do
-    use_real_time
-    sync "/osc/trigger/kick"
-    kick
+##| in_thread do
+##|   live_loop :kick do
+##|     use_real_time
+##|     sync "/osc/trigger/kick"
+##|     kick
+##|   end
+##| end
+
+
+##| in_thread do
+##|   live_loop :start do
+##|     use_real_time
+##|     sync "/osc/trigger/start"
+##|     start_record_audio
+##|   end
+##| end
+
+
+##| in_thread do
+##|   live_loop :stop do
+##|     use_real_time
+##|     sync "/osc/trigger/stop"
+##|     stop_record_audio
+##|   end
+##| end
+
+
+##| in_thread do
+##|   live_loop :play do
+##|     use_real_time
+##|     sync "/osc/trigger/play"
+##|     play_audio
+##|   end
+##| end
+
+# Attempt to use a more data-oriented approach but I'm
+# not sure how to pass around the method.
+
+osc_commands = [
+  {message: "/osc/trigger/kick",  fn: method(:kick)},
+  {message: "/osc/trigger/start", fn: method(:start_record_audio)},
+  {message: "/osc/trigger/stop",  fn: method(:stop_record_audio)},
+  {message: "/osc/trigger/play",  fn: method(:play_audio)},
+]
+
+osc_commands.each do |osc_command|
+  in_thread do
+    live_loop :stop do
+      use_real_time
+      sync osc_command[:message]
+      osc_command[:fn].()
+    end
   end
 end
 
 
-in_thread do
-  live_loop :start do
-    use_real_time
-    sync "/osc/trigger/start"
-    start_record_audio
-  end
-end
 
 
-in_thread do
-  live_loop :stop do
-    use_real_time
-    sync "/osc/trigger/stop"
-    stop_record_audio
-  end
-end
-
-
-in_thread do
-  live_loop :play do
-    use_real_time
-    sync "/osc/trigger/play"
-    play_audio
-  end
-end
-
-in_thread do
-  live_loop :logbuffers do
-    use_real_time
-    sync "/osc/trigger/logbuffers"
-    logbuffers
-  end
-end
 
 
 
