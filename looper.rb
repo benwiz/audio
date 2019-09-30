5eeb8a3c91b4d2998fb8a0662b27aab7503b6ee2
Ben Wiz <bwisialowski@gmail.com>
2019-09-29 21:47:09 -0500

    better state management and thread management

--- looper.rb
+++ looper.rb
@@ -3,22 +3,17 @@
 # someone else's attempt (check the gist):
 # https://in-thread.sonic-pi.net/t/betaversion-of-live-looper-with-touchosc-interface/379/2
 
-# TODO: `play` should automatically pick up new  tracks, calling it again shouldn't do anything
-# To complete this, I may want to send a message (using `cue`) from `stop` that kills then restarts the `play` thread
-# Since threads are named, it could be easy to kill a thread from the outside, maybe.
-
-
-# ~~ Only above must be done before I start using it.
-# ~~ Don't do the below until I've got it working consistently
-
+# TODO: Better named osc commands
+# TODO: More erganomic osc commands
 
 # TODO: Clean up thread use and make sure I'm not spawning tons of them
 # TODO: `start` osc command should be able to specify buffer
 # TODO: `pause` osc command should be able to specify buffer
 # TODO: new osc commands to control the metronome
-# TODO: osc commands should be namespaced better, even when it isn't recording
-# TODO (probably): Expose state via osc messages
-# TODO: Always pass live audio onward
+# TODO: Expose state by creating an api with osc
+# TODO: Use a logging library if printing to stdout causes performance issues
+# TODO: I may want to not use real time for the timing assistance, there's a name for this on other looping pedals
+# TODO***: There are several locations where I could take a more event-driven architecutre
 
 # FIXME: There is a minor audible glitch when looping. I'm pretty
 # certain this is a microphone hardware issue.
@@ -44,9 +39,57 @@ set :buffer_size, 32 # Large enough to hold whatever someone may record. # TODO:
 ############
 
 set :buffer_ids, []
-set :play, false
-set :record, false
+set :play, true
 live_audio :tunnel
+
+
+#################
+##|           |##
+##| Playback  |##
+##| Functions |##
+##|           |##
+#################
+
+
+def play_buffer_in_thread(buf)
+  in_thread do
+    if buf[:play] # Later, this may require grabbing the buffer from state again
+      loop do
+        if get(:play)
+          sample buffer(buf[:id], buf[:buffer_size])
+        else
+          stop # TODO: Probably want to avoid stopping threads for performance reasons
+        end
+        print "size=", buf[:size]
+        sleep buf[:size] # If 0, will throw error
+      end
+    end
+  end
+end
+
+
+def play_audio()
+  # When we are not playing
+  if get(:play) == false
+    set :play, true
+    
+    # buffer_size = get(:buffer_size) # TODO: I think this can be removed
+    buffer_ids = get(:buffer_ids)
+    print "buffer_ids=", buffer_ids
+    
+    # Play each buffer
+    buffer_ids.each do |buf|
+      play_buffer_in_thread buf
+    end
+    
+  end
+end
+
+
+def pause_audio()
+  print "buffer_ids=", get(:buffer_ids)
+  set :play, false
+end
 
 
 #################
@@ -74,9 +117,7 @@ end
 
 def start_record_audio()
   # When there is no recording currently happening
-  if get(:record) == false
-    set :record, true
-    
+  if get(:curr_buffer_id) == nil
     # Create a new buffer
     id = new_buffer_id
     set :curr_buffer_id, id
@@ -97,88 +138,53 @@ end
 
 
 def stop_record_audio()
-  # When the system is playing
-  if get(:record)
-    set :record, false
+  # When the system is recording into a buffer
+  if get(:curr_buffer_id)
     print "stop_record_audio"
     
-    # Stop audio and calculate size
+    # Stop audio and calculate size using clock time
     size = Time.now.to_f - get(:start_time)
     live_audio :recording, :stop
     size = size.floor(4)
-    print size
+    print "size=", size
     
-    # Start using the live_audio tunnel
+    # Start using the live_audio :tunnel
     live_audio :tunnel
     
-    # Update buffer_ids
-    buffer_ids = get(:buffer_ids)
-    curr_buffer_id = get(:curr_buffer_id)
-    print "curr_buffer_id=", curr_buffer_id
-    print "buffer_ids=", buffer_ids
-    new_buffer_ids = SonicPi::Core::SPVector.new(
-      buffer_ids.map do |b|
-        if b[:id] == curr_buffer_id
-          SonicPi::Core::SPMap.new(b.put(:size, size))
-        else
-          SonicPi::Core::SPMap.new(b)
-        end
-    end)
-    set :buffer_ids, new_buffer_ids
-  end
-end
-
-
-#################
-##|           |##
-##| Playback  |##
-##| Functions |##
-##|           |##
-#################
-
-
-def play_audio()
-  # When we are not playing
-  if get(:play) == false
-    set :play, true
+    buf = nil
     
-    # buffer_size = get(:buffer_size) # TODO: I think this can be removed
-    buffer_ids = get(:buffer_ids)
-    print "buffer_ids=", buffer_ids
-    
-    # Play each buffer
-    buffer_ids.each do |buf|
-      if buf[:size] > 0.5
-        in_thread do
-          if buf[:play]
-            loop do
-              if get(:play)
-                sample buffer(buf[:id], buf[:buffer_size])
-              else
-                stop # TODO: Probably want to avoid stopping threads
-              end
-              print "size=", buf[:size]
-              sleep buf[:size] # Should never be 0
-            end
+    # When bukffer is long enough)
+    if size > 0.5
+      # Update buffer_ids
+      buffer_ids = get(:buffer_ids)
+      curr_buffer_id = get(:curr_buffer_id)
+      print "curr_buffer_id=", curr_buffer_id
+      # TODO: There must be a better way, such as using index as id, and store a wav_id for the buffer's name
+      new_buffer_ids = SonicPi::Core::SPVector.new(
+        # The datatypes: https://github.com/hamstergem/hamster
+        buffer_ids.map do |b|
+          if b[:id] == curr_buffer_id
+            b = b.put(:size, size)
+            buf = b
+            SonicPi::Core::SPMap.new(b)
+          else
+            SonicPi::Core::SPMap.new(b)
           end
-        end
-      end
+      end)
+    end
+    
+    # Update global state
+    set :curr_buffer_id, nil
+    set :buffer_ids, new_buffer_ids
+    
+    # Start playing the new recording immediately
+    if buf # Probably unnecessary precaution
+      print "buf:", buf
+      play_buffer_in_thread buf
     end
   end
 end
 
-
-def pause_audio()
-  print "buffer_ids=", get(:buffer_ids)
-  set :play, false
-end
-
-
-def delete_buffers()
-  print "buffer_ids=", get(:buffer_ids)
-  set :buffer_ids, SonicPi::Core::SPVector[]
-  print "buffer_ids=", get(:buffer_ids)
-end
 
 
 #################
@@ -187,6 +193,13 @@ end
 ##| Functions |##
 ##|           |##
 #################
+
+
+def delete_buffers()
+  print "before=", get(:buffer_ids)
+  set :buffer_ids, SonicPi::Core::SPVector[]
+  print "after=", get(:buffer_ids)
+end
 
 
 def kick()
