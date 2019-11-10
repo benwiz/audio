67c3b884e60553394757f7c23ff3b8b9535fa7e2
Ben Wiz <bwisialowski@gmail.com>
2019-11-10 12:32:16 -0500

    remove second readme

--- app/src/app/dsp.clj
+++ app/src/app/dsp.clj
@@ -18,10 +18,9 @@
   (sh/sh sonic-pi-tool-path "stop"))
 
 
-
 ;; Start and stop the sonic pi server, not intending on automating yet
 (comment
-  (sh/sh "bash" "-c" "RBENV_VERSION=2.5.0 /Users/benwiz/.cargo/bin/sonic-pi-tool start-server" #_#_#_#_:env {:RBENV_VERSION "2.5.0" :HOME "/Users/benwiz/"})
+  (sh/sh "bash" "-c" "RBENV_VERSION=2.5.0 /Users/benwiz/.cargo/bin/sonic-pi-tool start-server" #_#_:env {:RBENV_VERSION "2.5.0" :HOME "/Users/benwiz/"})
   (sh/sh "/bin/echo $RBENV_VERSION" :env {:RBENV_VERSION "2.5.0" :HOME "/Users/benwiz/"})
   (sh/sh "/bin/ps -ef | grep 'ruby /Applications/Sonic Pi.app/server/ruby/bin/sonic-pi-server.rb' | grep -v grep | awk '{print $2}' | xargs kill -9")
 
