11851cb671c31a03438bdae6d8ee49f2fad5b855
Ben Wiz <bwisialowski@gmail.com>
2019-12-07 15:54:19 -0600

    only space

--- gui/src/gui/view.cljs
+++ gui/src/gui/view.cljs
@@ -15,12 +15,13 @@
           :className "pressed"
           :tabIndex "1"
           :onKeyDown (fn [e]
-                       (js/console.log "hit" (.random js/Math))
-                       #_(let [target (.. e -target)]
-                         (js/console.log target)
-                         (.. e -target (setAttribute "style" "height: 500px; width: 100%; background-color: #FF0000"))
-                         (js/setTimeout
-                          (fn []
-                            (target (setAttribute "style" "height: 500px; width: 100%; background-color: #FFDDDD")))
-                          500)))}
-         "Click this this box to start (this text wont change). Then press any key to trigger an osc command."))
+                       (when (= 32 (.. e -keyCode)) ;; space
+                         (js/console.log "hit" (.random js/Math)) ;; add a random so console prints a new line
+                         #_(let [target (.. e -target)]
+                             (js/console.log target)
+                             (.. e -target (setAttribute "style" "height: 500px; width: 100%; background-color: #FF0000"))
+                             (js/setTimeout
+                              (fn []
+                                (target (setAttribute "style" "height: 500px; width: 100%; background-color: #FFDDDD")))
+                              500))))}
+         "Click this this box to start (this text wont change). Then press SPACE to trigger an osc command."))
