f994250f9af0e660bd4fc6575718613f40cb3e46
Ben Wiz <bwisialowski@gmail.com>
2019-12-09 19:53:39 -0600

    try out some rust stuff

---
+++ experiment/dsp/src/main.rs
@@ -0,0 +1,29 @@
+use wavy::*;
+
+use std::collections::VecDeque;
+
+fn main() -> Result<(), AudioError> {
+    println!("Opening microphone system");
+    let mut mic = MicrophoneList::new(SampleRate::Normal)?;
+
+    println!("Opening speaker system");
+    let mut speaker = SpeakerList::new(SampleRate::Normal)?;
+
+    println!("Done");
+
+    let mut buffer = VecDeque::new();
+
+    loop {
+        mic.record(&mut |_index, l, r| {
+            buffer.push_back((l, r));
+        });
+
+        speaker.play(&mut || {
+            if let Some((lsample, rsample)) = buffer.pop_front() {
+                AudioSample::stereo(lsample, rsample)
+            } else {
+                AudioSample::stereo(0, 0)
+            }
+        });
+    }
+}
