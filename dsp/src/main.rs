8e3aaf62d36880fd55d2a912e3292ce29a013c31
Ben Wiz <bwisialowski@gmail.com>
2019-12-18 19:18:01 -0600

    kind of working

--- dsp/src/main.rs
+++ dsp/src/main.rs
@@ -9,7 +9,7 @@ use std::fs;
 use std::io::Error;
 use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
 use rocket::State;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::sync::atomic::{Ordering, AtomicBool, AtomicI32};
 
 const LATENCY_MS: f32 = 20.0;
@@ -18,127 +18,8 @@ const PATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/recordings");
 struct AppState {
     status: AtomicBool,
     count: AtomicI32,
-}
-
-fn looper(app_state: Arc<AppState>) -> Result<(), failure::Error> {
-    let host = cpal::default_host();
-    let event_loop = host.event_loop();
-
-    // List devices
-    println!("Devices:");
-    let devices = host.devices()?;
-    for (device_index, device) in devices.enumerate() {
-        println!("  {}. \"{}\"", device_index, device.name()?);
-    }
-
-    // Default devices.
-    let input_device = host.default_input_device().expect("failed to get default input device");
-    let output_device = host.default_output_device().expect("failed to get default output device");
-    println!("Using default input device: \"{}\"", input_device.name()?);
-    println!("Using default output device: \"{}\"", output_device.name()?);
-
-    // We'll try and use the same format between streams to keep it simple
-    let mut format = input_device.default_input_format()?;
-    format.data_type = cpal::SampleFormat::F32;
-
-    // Build streams.
-    println!("Attempting to build both streams with `{:?}`.", format);
-    let input_stream_id = event_loop.build_input_stream(&input_device, &format)?;
-    let output_stream_id = event_loop.build_output_stream(&output_device, &format)?;
-    println!("Successfully built streams.");
-
-    // Create a delay in case the input and output devices aren't synced.
-    let latency_frames = (LATENCY_MS / 1_000.0) * format.sample_rate.0 as f32;
-    let latency_samples = latency_frames as usize * format.channels as usize;
-
-    // The channel to share samples.
-    let (tx, rx) = std::sync::mpsc::sync_channel(latency_samples * 2);
-
-    // Fill the samples with 0.0 equal to the length of the delay.
-    for _ in 0..latency_samples {
-        tx.send(0.0)?;
-    }
-
-    // Play the streams.
-    println!("Starting the input and output streams with `{}` milliseconds of latency.", LATENCY_MS);
-    event_loop.play_stream(input_stream_id.clone())?;
-    event_loop.play_stream(output_stream_id.clone())?;
-
-    // The WAV file we're recording to.
-    const FILEPATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/recordings/0.wav"); // TODO: Use the global PATH
-    let spec = wav_spec_from_format(&format);
-    let writer = hound::WavWriter::create(FILEPATH, spec)?;
-    let writer = std::sync::Arc::new(std::sync::Mutex::new(Some(writer)));
-    let writer_2 = writer.clone();
-    let app_state_clone = app_state.clone();
-
-    // Run the event loop on a separate thread.
-    std::thread::spawn(move || {
-        event_loop.run(move |id, result| {
-            let data = match result {
-                Ok(data) => data,
-                Err(err) => {
-                    eprintln!("an error occurred on stream {:?}: {}", id, err);
-                    return;
-                }
-            };
-
-            match data {
-                cpal::StreamData::Input { buffer: cpal::UnknownTypeInputBuffer::F32(buffer) } => {
-                    assert_eq!(id, input_stream_id);
-                    let mut output_fell_behind = false;
-
-                    let recording_status = app_state_clone.status.load(Ordering::Relaxed);
-                    // println!("recording: {:?}", recording_status);
-
-                    // Write to file (NOTE: iterating through the buffer 2x is not ideal)
-                    if recording_status {
-                        if let Ok(mut guard) = writer_2.try_lock() {
-                            if let Some(writer) = guard.as_mut() {
-                                for &sample in buffer.iter() {
-                                    writer.write_sample(sample).ok();
-                                }
-                            }
-                        }
-                    }
-
-                    // Write to output stream
-                    for &sample in buffer.iter() {
-                        if tx.try_send(sample).is_err() {
-                            output_fell_behind = true;
-                        }
-                    }
-                    if output_fell_behind {
-                        eprintln!("output stream fell behind: try increasing latency");
-                    }
-                },
-                cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
-                    assert_eq!(id, output_stream_id);
-                    let mut input_fell_behind = None;
-                    for sample in buffer.iter_mut() {
-                        *sample = match rx.try_recv() {
-                            Ok(s) => s,
-                            Err(err) => {
-                                input_fell_behind = Some(err);
-                                0.0
-                            },
-                        };
-                    }
-                    if let Some(err) = input_fell_behind {
-                        eprintln!("input stream fell behind: {}: try increasing latency", err);
-                    }
-                },
-                _ => panic!("we're expecting f32 data"),
-            }
-        });
-    });
-
-    // Run for MAX seconds before closing (basically, forever)
-    let seconds = std::u64::MAX;
-    println!("Start audio processor (run for {} seconds)", seconds);
-    std::thread::sleep(std::time::Duration::new(seconds, 0));
-    writer.lock().unwrap().take().unwrap().finalize()?; // TODO: Will need to call this when I stop recording, I think. That means writer probably has to go in the AppState
-    Ok(())
+    // writer: Mutex<hound::WavWriter<std::io::BufWriter<std::fs::File>>>,
+    writer: Mutex<std::option::Option<hound::WavWriter<std::io::BufWriter<std::fs::File>>>>,
 }
 
 fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
@@ -170,7 +51,7 @@ fn trigger(app_state: State<Arc<AppState>>) -> String {
     let response = match curr_status {
         true => {
             app_state.status.store(false, Ordering::Relaxed);
-            // TODO: stop recording
+            app_state.writer.lock().unwrap().take().unwrap().finalize().expect("Error finalizing writer");// stop wav writer
             "stop recording"
         },
         false => {
@@ -218,8 +99,7 @@ fn delete_dir_contents(read_dir_res: Result<fs::ReadDir, Error>) {
     };
 }
 
-fn main() {
-
+fn main() -> Result<(), failure::Error> {
     // Create the recordings dir incase it doesn't exist
     create_dir(PATH);
 
@@ -227,16 +107,130 @@ fn main() {
     let recordings_dir = fs::read_dir(PATH);
     delete_dir_contents(recordings_dir);
 
+    // Get audio host and audio event loop
+    let host = cpal::default_host();
+    let event_loop = host.event_loop();
+
+    // List devices
+    println!("Devices:");
+    let devices = host.devices()?;
+    for (device_index, device) in devices.enumerate() {
+        println!("  {}. \"{}\"", device_index, device.name()?);
+    }
+
+    // Default devices.
+    let input_device = host.default_input_device().expect("failed to get default input device");
+    let output_device = host.default_output_device().expect("failed to get default output device");
+    println!("Using default input device: \"{}\"", input_device.name()?);
+    println!("Using default output device: \"{}\"", output_device.name()?);
+
+    // We'll try and use the same format between streams to keep it simple
+    let mut format = input_device.default_input_format()?;
+    format.data_type = cpal::SampleFormat::F32;
+
+    // Build streams.
+    println!("Attempting to build both streams with `{:?}`.", format);
+    let input_stream_id = event_loop.build_input_stream(&input_device, &format)?;
+    let output_stream_id = event_loop.build_output_stream(&output_device, &format)?;
+    println!("Successfully built streams.");
+
+    // Create a delay in case the input and output devices aren't synced.
+    let latency_frames = (LATENCY_MS / 1_000.0) * format.sample_rate.0 as f32;
+    let latency_samples = latency_frames as usize * format.channels as usize;
+
+    // The channel to share samples.
+    let (tx, rx) = std::sync::mpsc::sync_channel(latency_samples * 2);
+
+    // Fill the samples with 0.0 equal to the length of the delay.
+    for _ in 0..latency_samples {
+        tx.send(0.0)?;
+    }
+
+    // Play the streams.
+    println!("Starting the input and output streams with `{}` milliseconds of latency.", LATENCY_MS);
+    event_loop.play_stream(input_stream_id.clone())?;
+    event_loop.play_stream(output_stream_id.clone())?;
+
+    // The WAV file we're recording to.
+    const FILEPATH: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/recordings/0.wav"); // TODO: Use the global PATH
+    let spec = wav_spec_from_format(&format);
+    let writer = hound::WavWriter::create(FILEPATH, spec)?;
+
     // Initialize app state
     let app_state = Arc::new(AppState {
         status: AtomicBool::new(false),
         count: AtomicI32::new(0),
+        writer: Mutex::new(Some(writer)),
     });
     let app_state_clone = app_state.clone();
 
+    // Run the event loop on a separate thread.
     std::thread::spawn(move || {
-        looper(app_state_clone).expect("Error running looper");
+        event_loop.run(move |id, result| {
+            let data = match result {
+                Ok(data) => data,
+                Err(err) => {
+                    eprintln!("an error occurred on stream {:?}: {}", id, err);
+                    return;
+                }
+            };
+
+            match data {
+                cpal::StreamData::Input { buffer: cpal::UnknownTypeInputBuffer::F32(buffer) } => {
+                    assert_eq!(id, input_stream_id);
+                    let mut output_fell_behind = false;
+
+                    let recording_status = app_state_clone.status.load(Ordering::Relaxed);
+                    // println!("recording: {:?}", recording_status);
+
+                    // Write to file (NOTE: iterating through the buffer 2x is not ideal, this is a problem for later)
+                    if recording_status {
+                        if let Ok(mut guard) = app_state_clone.writer.try_lock() {
+                            if let Some(writer) = guard.as_mut() {
+                                for &sample in buffer.iter() {
+                                    writer.write_sample(sample).ok();
+                                }
+                            }
+                        }
+                    }
+
+                    // Write to output stream
+                    for &sample in buffer.iter() {
+                        if tx.try_send(sample).is_err() {
+                            output_fell_behind = true;
+                        }
+                    }
+                    if output_fell_behind {
+                        eprintln!("output stream fell behind: try increasing latency");
+                    }
+                },
+                cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::F32(mut buffer) } => {
+                    assert_eq!(id, output_stream_id);
+                    let mut input_fell_behind = None;
+                    for sample in buffer.iter_mut() {
+                        *sample = match rx.try_recv() {
+                            Ok(s) => s,
+                            Err(err) => {
+                                input_fell_behind = Some(err);
+                                0.0
+                            },
+                        };
+                    }
+                    if let Some(err) = input_fell_behind {
+                        eprintln!("input stream fell behind: {}: try increasing latency", err);
+                    }
+                },
+                _ => panic!("we're expecting f32 data"),
+            }
+        });
     });
 
+
+    // Start a blocking http server
     server(app_state);
+
+    // writer.lock().unwrap().take().unwrap().finalize()?; // TODO: Will need to call this when I stop recording, I think. That means writer probably has to go in the AppState
+
+    // Never returns
+    Ok(())
 }
